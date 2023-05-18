67497cea8f0faff3ab966e17d7b20507e91ee6f9
vishalkumar9 <vishal091200kumar@gmail.com>
2023-05-18 13:45:31 +0530

    modified About us page

--- frontend/src/components/about/About.js
+++ frontend/src/components/about/About.js
@@ -7,42 +7,29 @@ import './About.css';
 import im1 from "../image/im1.png";
 import im2 from "../image/im2.png";
 import im3 from "../image/im3.png";
+import DevloperCard from "../card/DevloperCard";
 
 
 const About = () => {
     return (
         <div className="aboutus">
-            <div className="div1">
-                <div className="text">
-                    <h1>Our Story</h1>
-                    <h3>"Our project was born out of a shared vision among four dedicated individuals. As a major project, we set out to create something special,
-                        something that would make a difference in the world of real estate. With a passion for innovation and a commitment
-                        to excellence, we brought together our diverse backgrounds and skillsets to make this vision a reality.
-                        Today, we are proud to offer a unique and valuable resource to our clients, one that is the result of the hard work and dedication of our team."</h3>
-                </div>
-                {/*<div className="div1_image">*/}
-                {/*    <img src={website} alt=""/>*/}
-                {/*</div>*/}
+            <div className="heading">
+                <h1>About Us</h1>
             </div>
-            <div className="services">
-                <div className="heading" >
-                    <h1>Services We Provide</h1>
-                </div>
-                <div className="display_services">
-                    <FeatureCard image = {im1} heading = "Rent" content="Find your ideal rental property with our extensive selection of homes, apartment and many more."/>
-                    <FeatureCard image = {im2} heading = "Sell" content="Find your dream home at a dream price with our exclusive selection of sale listings. Our team is dedicated to helping you find the best deal possible."/>
-                    <FeatureCard image = {im3} heading = "Listing" content="Get the exposure you need to sell your property with our free listing service. Our platform makes it easy to list your property and get it in front of qualified buyers"/>
+            <div className="main_aboutus">
+                <div className="heading1"><h2>Who We Are</h2></div>
+                <p>
+                    At Our website, we are passionate about revolutionizing the way people experience real estate. Our team of dedicated professionals is committed to providing exceptional services and delivering unparalleled value to our clients. With a deep understanding of the industry and a forward-thinking approach, we aim to create a seamless and rewarding real estate journey for all.
+                </p>
+                <div className="Founders">
+                    <h2>Meet Our Devloper</h2>
+                    <div className="container">
+                        <DevloperCard name="Vishal Kumar Singh" description="Our Lead Developer of this real estate website, responsible for bringing the vision to life through code. Together with his team, they have created a seamless platform that revolutionizes the way people engage with real estate online."/>
+                        <DevloperCard name ="Abhishek Negi" description="Dedicated Developer and Researcher on this real estate website, working alongside the team to bring innovation and functionality to life. By leveraging his coding expertise and conducting thorough research, he has contributed to creating a dynamic platform that empowers users with comprehensive property information."/>
+                        <DevloperCard name = "Ayush Barakoti" description="Dedicated devloper on this real estate website, working alongside the team to bring innovation and functionality to life.Together, they have crafted a user-centric solution that revolutionizes the way individuals interact with the real estate market."/>
+                    </div>
                 </div>
             </div>
-            {/*<div className="intro">*/}
-            {/*    <div className="heading" >*/}
-            {/*        <h1>Meet Our Devlopers</h1>*/}
-            {/*    </div>*/}
-            {/*    /!*<div className="display_dev">*!/*/}
-            {/*    /!*    <img className="dev" src={vishal} alt=""/>*!/*/}
-            {/*    /!*    <h3>Full Stack Devloper</h3>*!/*/}
-            {/*    /!*</div>*!/*/}
-            {/*</div>*/}
         </div>
     );
 }
