45516de58d1a9c0598ecb90ef525e3596a00618e
vishalkumar9 <vishal091200kumar@gmail.com>
2023-10-25 18:23:26 +0530

    update navbar

--- frontend/src/components/navigations/MainNavigation.js
+++ frontend/src/components/navigations/MainNavigation.js
@@ -1,15 +1,12 @@
 import React, {useContext, useState} from "react";
-import {Link, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 import {AuthContext} from "../context/AuthContext";
-import logo from "../image/logo.svg"
+
 
 import "./MainNavigation.css";
 
 import { useMediaQuery } from 'react-responsive'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faXmark, faBars} from "@fortawesome/free-solid-svg-icons";
-import Dropdown from 'react-bootstrap/Dropdown';
 
 
 const  MainNavigation = (props) => {
@@ -34,56 +31,38 @@ const AuthC = useContext(AuthContext);
         AuthC.logout();
     }
 
+    return(
+        <nav className="navbar bg-body-tertiary fixed-top custom_theme_navbar">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">Real Estate</a>
+                <button className="navbar-toggler custom_theme_navbar_btn" type="button" data-bs-toggle="offcanvas"
+                        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="offcanvas offcanvas-end custom_theme_navbar" tabIndex="-1" id="offcanvasNavbar"
+                     aria-labelledby="offcanvasNavbarLabel">
+                    <div className="offcanvas-header">
+                        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Real Estate</h5>
+                        <button type="button" className="btn-close custom_theme_navbar_btn" data-bs-dismiss="offcanvas"
+                                aria-label="Close"></button>
+                    </div>
+                    <div className="offcanvas-body">
+                        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                            {props.routes.map((routeDetails) => (
+                                <li className="nav-item">
+                                    <a className="nav-link" aria-current="page" href={routeDetails.url}>{routeDetails.content}</a>
+                                </li>
+                            ))}
+                            {AuthC.isLoggedIn && <li className="nav-item">
+                                <a className="nav-link" aria-current="page" onClick={handleLogout}>Logout</a>
+                            </li>}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
 
-    if(isMobile){
-        return (
-            <Dropdown>
-            <div className="main-navi-div">
-                <nav>
-                    <div>
-                        <img src = {logo} alt=""/>
-                    </div>
-                    <div>
-                        <ul className={isClicked ? "navbar active" : "navbar"}>
-                            {navLinks.map((item,i)=>(
-                                <li key = {i} className={!i && "active"}>
-                                    { item.content==="Logout" &&  <Link onClick = {handleLogout}>{item.content}</Link> }
-                                    { item.content!=="Logout" &&  <Link to={item.url} onClick={() => setIsClicked(false)}>{item.content}</Link> }
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className="mobile">
-                        <div onClick={handleClick}>
-                            {!isClicked ? <FontAwesomeIcon icon={faBars}/> : <FontAwesomeIcon icon={faXmark}/>}
-                        </div>
-                    </div>
-                </nav>
-            </div>
-            </Dropdown>
-        );
-    }
-    else {
-        return (
-            <div>
-                <nav>
-                    <div>
-                        <img src = {logo}/>
-                    </div>
-                    <div>
-                        <ul className="navbar">
-                            {navLinks.map((item,i)=>(
-                                <li key = {i} className={!i && "active"}>
-                                    { item.content==="Logout" &&  <Link onClick = {handleLogout}>{item.content}</Link> }
-                                    { item.content!=="Logout" && <Link to={item.url}>{item.content}</Link> }
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        );
-    }
+    );
 }
 
 export default MainNavigation;
